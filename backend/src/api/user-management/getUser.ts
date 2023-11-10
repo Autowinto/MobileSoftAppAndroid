e7f6a0ad2e6a59fbb243e1fd0f5e13e2fcebb7f6
Aloxion <ansan21@student.sdu.dk>
2023-11-10 16:01:52 +0100

    Backend Setup

---
+++ backend/src/api/user-management/getUser.ts
@@ -0,0 +1,70 @@
+import { Application, Request, Response } from 'express';
+import { OpenApi, Types } from 'ts-openapi';
+
+export async function getUser(req: Request, res: Response) {
+    // const { id } = req.query;
+    // const user = await db.user.findOne({ where: { id } });
+    // res.json(user);
+
+    // I just want to see if this works
+    res.send({ "name": "John" });
+}
+
+export function initGetUser(app: Application, openApi: OpenApi) {
+    app.get('/get_user', getUser)
+
+    openApi.addPath(
+        '/get_user',
+        {
+            get: {
+                summary: 'Get User',
+                description: 'This operation gets a User',
+                operationId: 'get-User-op',
+                requestSchema: {
+                    headers: {
+                        requestId: Types.String({
+                            description: 'Request ID',
+                            required: false,
+                            example: 'b710e129-4e2c-4448-b605-73b18d297bae'
+                        })
+                    },
+                    query: {
+                        id: Types.Uuid({
+                            description: 'User ID',
+                            required: true,
+                            example: 'b710e129-4e2c-4448-b605-73b18d297bae'
+                        })
+                    }
+                },
+                tags: ['User Operations'],
+                responses: {
+                    200: {
+                        description: 'OK',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'object',
+                                    description: 'User data',
+                                    properties: {
+                                        name: {
+                                            type: 'string',
+                                            description: 'User name',
+                                            maxLength: 100,
+                                        },
+                                        type: {
+                                            type: 'array',
+                                            description: 'User type',
+                                            example: ['Platinum', 'Gold', 'Silver', 'Bronze'],
+                                        }
+                                    },
+                
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        true
+    )
+}
---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+import { getUser, initGetUser } from "./api/user-management/getUser";
+import * as managementRouter from './api/user-management/router';
+import { initCreateUser } from "./api/user-management/createUser";
+import { initOpenApi, openApiInstance } from "./openapi";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+app.use('/api', managementRouter.default);
+
+initCreateUser(app, openApiInstance);
+initGetUser(app, openApiInstance);
+initOpenApi(app, openApiInstance);
+
+// Testing
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`, `http://${process.env.HOST}:${process.env.PORT}`);
+});
