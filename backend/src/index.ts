79b5984ea9f4911eb7616986ca91b7cd23237adc
Kim JE Lassen <kimjelassen@yahoo.com>
2023-11-12 13:23:58 +0100

    MongoDB setup + errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { getUser, initGetUser } from "./api/user-management/getUser";
 import * as managementRouter from './api/user-management/router';
 import { initCreateUser } from "./api/user-management/createUser";
 import { initOpenApi, openApiInstance } from "./openapi";
+import * as db from "./db/dbService";
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,9 @@ app.use('/api', managementRouter.default);
 initCreateUser(app, openApiInstance);
 initGetUser(app, openApiInstance);
 initOpenApi(app, openApiInstance);
+
+db.run();
+db.get();
 
 // Testing
 app.listen(process.env.PORT, () => {
