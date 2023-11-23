7d5b3513ba44203c08926d1522fa5032c5b871a1
Magnus Christian Larsen <winto@MacBook.local>
2023-11-23 14:58:14 +0100

    added retrofit dependency

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 
 dependencies {
     val nav_version = "2.7.5"
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -61,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.compose.material3:material3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
