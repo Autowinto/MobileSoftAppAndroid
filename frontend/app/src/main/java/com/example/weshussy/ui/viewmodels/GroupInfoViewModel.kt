5ad41c673d731c983db9c7cb3a55e4ad2a99ca6c
Magnus Christian Larsen <winto@MacBook.local>
2023-11-24 21:29:19 +0100

    Fixed a wrongfully defined data class

--- frontend/app/src/main/java/com/example/weshussy/ui/viewmodels/GroupInfoViewModel.kt
+++ frontend/app/src/main/java/com/example/weshussy/ui/viewmodels/GroupInfoViewModel.kt
@@ -3,11 +3,11 @@ package com.example.weshussy.ui.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weshussy.api.RetrofitClient
-import com.example.weshussy.api.interfaces.GroupCreationRequest
+import com.example.weshussy.api.interfaces.GroupApi.GroupCreateRequestBody
 
 class GroupInfoViewModel(): ViewModel() {
     suspend fun createGroup(groupName: String, userId: String) {
-        val request = GroupCreationRequest(group_name = groupName,  user_id = userId)
+        val request = GroupCreateRequestBody(name = groupName, userId = userId)
         RetrofitClient().groupApi.createGroup(request)
     }
 }
