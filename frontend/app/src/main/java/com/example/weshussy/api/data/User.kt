6c56e008c8758d1239a417f2520dc4f1b2e5f3cb
Magnus Christian Larsen <happymansi@hotmail.com>
2023-11-26 00:17:58 +0100

    Updated retrofit data class to fit backend

--- frontend/app/src/main/java/com/example/weshussy/api/data/User.kt
+++ frontend/app/src/main/java/com/example/weshussy/api/data/User.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class User(
     @JsonProperty("id") val id: String,
-    @JsonProperty("first_name") val firstName: String,
-    @JsonProperty("last_name") val lastName: String,
+    @JsonProperty("firstName") val firstName: String,
+    @JsonProperty("lastName") val lastName: String,
     @JsonProperty("email") val email: String,
     @JsonProperty("phoneNmb") val phoneNmb: String,
     @JsonProperty("password") val password: String,
