5993aeb80088a45b677424be6087dc46b8ac698c
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-11-27 16:25:30 +0100

    Added edit group settings and did some restructuring

--- frontend/app/src/main/java/com/example/weshussy/api/interfaces/GroupApi.kt
+++ frontend/app/src/main/java/com/example/weshussy/api/interfaces/GroupApi.kt
@@ -7,38 +7,57 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 import java.lang.invoke.TypeDescriptor
 
 interface GroupApi {
+
+    // GROUPS MANAGEMENT
     data class GroupCreateRequestBody (
         val name: String,
         val userId: String,
         val description: String
     )
-    @POST("groups/")
-    suspend fun createGroup(@Body requestBody: GroupCreateRequestBody): Response<Object>
 
-    @GET("groups/{id}")
-    suspend fun getGroupById(@Path("id") id: String): Response<Group>
+    data class GroupUpdateRequestBody (
+        val id: String,
+        val name: String,
+        val userId: String,
+        val description: String
+    )
 
     data class GroupDeleteRequestBody (
         val id: String
     )
+
+    @POST("groups/")
+    suspend fun createGroup(@Body requestBody: GroupCreateRequestBody): Response<Object>
+
+    @PUT("groups/")
+    suspend fun updateGroup(@Body requestBody: GroupUpdateRequestBody): Response<String>
+
+    @GET("groups/{id}")
+    suspend fun getGroupById(@Path("id") id: String): Response<Group>
+
     @DELETE("groups/")
     suspend fun deleteGroup(@Body requestBody: GroupDeleteRequestBody): Response<String>
 
+
+    // GROUP MEMBER MANAGEMENT
     data class GroupMemberCreateRequestBody (
         val name: String,
         val userId: String
     )
-    @POST("groups/members/")
-    suspend fun addGroupMember(@Body requestBody: GroupMemberCreateRequestBody): Response<String>
 
     data class GroupMemberDeleteRequestBody (
         val id: String
     )
+
+    @POST("groups/members/")
+    suspend fun addGroupMember(@Body requestBody: GroupMemberCreateRequestBody): Response<String>
+
     @DELETE("groups/members/")
     suspend fun removeMember(@Body requestBody: GroupMemberDeleteRequestBody): Response<String>
 
