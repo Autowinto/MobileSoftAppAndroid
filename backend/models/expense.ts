b6021d695cf4732c1b76fc600078974a7111a2e0
Magnus Christian Larsen <happymansi@hotmail.com>
2023-12-21 23:21:12 +0100

    Added

--- backend/models/expense.ts
+++ backend/models/expense.ts
@@ -1,22 +1,23 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../src/initialize';
+import { DataTypes } from "sequelize";
+import { sequelize } from "../src/initialize";
 
-const Expense = sequelize.define(
-    'expense', {
-    id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-    },
-    userId: {
-        type: DataTypes.STRING,
-    },
-    groupId: {
-        type: DataTypes.STRING,
-    },
-    amount: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-},
-);
+const Expense = sequelize.define("expense", {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  userId: {
+    type: DataTypes.STRING,
+  },
+  groupId: {
+    type: DataTypes.STRING,
+  },
+  amount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 export default Expense;
